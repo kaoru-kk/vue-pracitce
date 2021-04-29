6a2a22412ecd75524bdfe425e0cf32d21b411ec8
kaoru-kk <kkaoru1998@gmail.com>
2021-04-29 17:02:12 +0900

    renderとか仮想DOM

--- main.js
+++ main.js
@@ -22,5 +22,24 @@ var vm2 = new Vue({
   template: '<h1>hello {{ name }}</h1>'
 })
 
-// $mount (elプロパティの代わり)
-// vm.$mount('#app1')
+new Vue({
+  data: {
+    name: 'kaoru'
+  },
+  // templateとかも全てrenderに変換されている（全て仮想DOMに）
+  render: function(h) {
+    // hと描かれることが多い
+    // 仮想DOMを作る
+    // 仮想ノードを返すもの
+    console.log(h('h1', 'g_a ' + this.name) ) ;
+    return h('h1', 'g_a ' + this.name );
+  }
+}).$mount('#app3')
+
+// hはdocument.createElementとは違う
+// DOMを作る
+var div = document.createElement('div')
+// DOM
+console.log(document)
+console.dir(document)
+console.log(div)
