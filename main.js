cfcd550986d52d2866cad7d3f9ae26e5a829bbcf
kaoru-kk <kkaoru1998@gmail.com>
2021-04-29 15:31:30 +0900

    v-for

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 new Vue({
   el: '#app',
   data: {
-    ok: true
+    fruits: ['apple', 'banana', 'grape'],
+    object: {
+      firstName: 'tarou',
+      lastName: 'yamada',
+      age: 21
+    }
+  },
+  methods: {
+    removeForm: function(){
+      this.fruits.shift()
+    }
   }
 })
