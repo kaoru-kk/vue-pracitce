9d4a2bd5f5f38ed4ed0719835352e2865f732ca1
kaoru-kk <kkaoru1998@gmail.com>
2021-04-29 16:22:49 +0900

    jsぽくかける

--- main.js
+++ main.js
@@ -1,28 +1,13 @@
+// これもできる
+var data = {
+  message: 'instance A'
+}
+
 var vm1 = new Vue({
   el: '#app1',
-  data: {
-    message: 'instance A'
-  }
+  data: data
 })
-// 外側から書き換えられる
-vm1.message = 'value is change';
-// 後付けできるけど、再更新はできない（リアクティブシステムが関係）
-// vueインスタンスを作成したときに、ゲッターとセッターを用意
-// 後付けしたらこれらが用意されないから再更新不可
-vm1.name = 'hogehoge'
-// nameのゲッターとセッターがないこと確認
-console.log(vm1)
 
-// 2個作れる（あまりしない方がいいけど）
-var vm2 = new Vue({
-  el: '#app2',
-  data: {
-    message: 'instance B'
-  },
-  methods:{
-    changeInstance: function() {
-      // vm2からvm1の書き換え
-      vm1.message = 'change from vm2'
-    }
-  }
-})
+// $ つける（data）
+
+console.log(vm1.$data);
