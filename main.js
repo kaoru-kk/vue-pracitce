343b1c5f6c00f1767f784df8bf3f28a62fad4dc7
kaoru-kk <kkaoru1998@gmail.com>
2021-04-29 15:57:41 +0900

    リアクティブシステムの関連

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-new Vue({
-  el: '#app',
+var vm1 = new Vue({
+  el: '#app1',
   data: {
-    fruits: ['apple', 'banana', 'grape'],
-    object: {
-      firstName: 'tarou',
-      lastName: 'yamada',
-      age: 21
-    }
+    message: 'instance A'
+  }
+})
+// 外側から書き換えられる
+vm1.message = 'value is change';
+// 後付けできるけど、再更新はできない（リアクティブシステムが関係）
+// vueインスタンスを作成したときに、ゲッターとセッターを用意
+// 後付けしたらこれらが用意されないから再更新不可
+vm1.name = 'hogehoge'
+// nameのゲッターとセッターがないこと確認
+console.log(vm1)
+
+// 2個作れる（あまりしない方がいいけど）
+var vm2 = new Vue({
+  el: '#app2',
+  data: {
+    message: 'instance B'
   },
-  methods: {
-    removeForm: function(){
-      this.fruits.shift()
+  methods:{
+    changeInstance: function() {
+      // vm2からvm1の書き換え
+      vm1.message = 'change from vm2'
     }
   }
 })
