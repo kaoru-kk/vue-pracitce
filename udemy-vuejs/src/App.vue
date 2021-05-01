cad4a8f08d804ec11ac9cd2a8bdd1430e0269105
kaoru-kk <kkaoru1998@gmail.com>
2021-05-01 12:34:34 +0900

    単一コンポーネントのimport

--- udemy-vuejs/src/App.vue
+++ udemy-vuejs/src/App.vue
@@ -1,24 +1,3 @@
 <template>
-  <p>いいね！{{ fav_number }}</p>
+  <LikeNumber></LikeNumber>
 </template>
-
-<script>
-export default {
-  // es5
-  // data: function(){
-  //   return {
-  //     fav_number: 100
-  //   }
-  // }
-
-  // es6
-  data() {
-    return {
-      fav_number: 19
-    };
-  }
-}
-</script>
-
-<style>
-</style>
---
+++ udemy-vuejs/src/LikeNumber.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>いいね！{{ fav_number }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fav_number: 19
+    };
+  }
+}
+</script>
